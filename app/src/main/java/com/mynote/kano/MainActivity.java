110ba38282a4867d945a9b433b75c5b7beff27f5
jihye7788 <43394982+jihye7788@users.noreply.github.com>
2019-06-15 22:53:55 +0900

    Revert "190615_1"
    This reverts commit 6df98a6f1d9d1c591129491effcd5471762f959a.

--- app/src/main/java/com/mynote/kano/MainActivity.java
+++ app/src/main/java/com/mynote/kano/MainActivity.java
@@ -14,11 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       /* // Write a message to the database
+        // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Hello, World!");*/
+        myRef.setValue("Hello, World!");
     }
 
 

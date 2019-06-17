90b6537c050e74c25440ebb8a822cc36fb438cbe
jihye7788 <43394982+jihye7788@users.noreply.github.com>
2019-06-17 12:55:36 +0900

    190617_2

--- app/src/main/java/com/mynote/kano/calendarActivity.java
+++ app/src/main/java/com/mynote/kano/calendarActivity.java
@@ -1,5 +1,6 @@
 package com.mynote.kano;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,6 +16,8 @@ public class calendarActivity extends AppCompatActivity {
 
     DatePicker datePicker;  //  datePicker - 날짜를 선택하는 달력
     String pickedDate;
+
+    public static final int REQUEST_CODE_MENU = 101;//diary에서 받는 코드
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +52,27 @@ public class calendarActivity extends AppCompatActivity {
 
             }
         });
+    }
 
+    public void goDiary(View v){
+        Intent intent = new Intent();
+        intent.putExtra("date","pickedDate");
+        startActivityForResult(intent, REQUEST_CODE_MENU);
+    }
 
+    //writeDiary에서 처리된 결과를 받는 메소드
+    //처리된 결과 코드 (resultCode)가 REQUEST_CODE_MENU랑 같으면 ㄱㄱ
+    //writeDiary에서 일기를 썼으면 위에 점을 찍어줌
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(resultCode==REQUEST_CODE_MENU){
+            switch (requestCode){
+                case 101:
+               /* 캘린더에 점 찍어주는거 따로 하기
+               * */
+                    break;
+            }
+        }
     }
 }

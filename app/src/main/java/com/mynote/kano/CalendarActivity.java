18dc9a6eb9063625b11b1e3faecbf70baffa66d3
uiseon <uiseon92@gmail.com>
2019-06-17 16:27:45 +0900

    Commit List

---
+++ app/src/main/java/com/mynote/kano/CalendarActivity.java
@@ -0,0 +1,18 @@
+package com.mynote.kano;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalendarActivity extends AppCompatActivity {
+    //MaterialCalendarView materialCalendarView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+      //  materialCalendarView = findViewById(R.id.calendarView);
+
+    }
+
+
+}

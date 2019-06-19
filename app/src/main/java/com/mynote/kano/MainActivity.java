43b89028ab61d75319472c60b9febbc80c886fb5
SJ <jshy826@gmail.com>
2019-06-19 10:47:29 +0900

    loginpage
    make up loginpage

--- app/src/main/java/com/mynote/kano/MainActivity.java
+++ app/src/main/java/com/mynote/kano/MainActivity.java
@@ -1,17 +1,22 @@
 package com.mynote.kano;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         Button loginpage_button = (Button) findViewById(R.id.gitLoginButton);
         loginpage_button.setOnClickListener(new View.OnClickListener(){

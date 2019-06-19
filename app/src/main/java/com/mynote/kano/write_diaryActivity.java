41c02ca0e5b40fceaf46623603558fa888a36817
jihye7788 <43394982+jihye7788@users.noreply.github.com>
2019-06-19 21:31:57 +0900

    190619_4

--- app/src/main/java/com/mynote/kano/write_diaryActivity.java
+++ app/src/main/java/com/mynote/kano/write_diaryActivity.java
@@ -15,7 +15,7 @@ import android.widget.TextView;
 
 public class write_diaryActivity extends AppCompatActivity {
 
-    private EditText diaryContent;   //  fileName - 돌고 도는 선택된 날짜의 파일 이름
+    private EditText diaryContent =(EditText)findViewById(R.id.diaryContent);
     private String diaryDate;
     private String userId;
 
@@ -28,23 +28,24 @@ public class write_diaryActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Intent intent = getIntent();
-
-        if(intent != null) {
-            diaryDate = intent.getExtras().getString("diaryDate");
+        diaryDate = intent.getExtras().getString("diaryDate");
+        userId = intent.getExtras().getString("userId");
+        if(diaryDate!= null) {
             TextView tx1 =findViewById(R.id.dateView);
             tx1.setText(diaryDate);
         }
 
-
-
-      /* myRef.setValue(diaryContent.getText().toString());*/
     }
 
     private void writeNewDiary(String userId, String diaryDate, String diaryContent){
-        User usersDiary = new User(userId, diaryDate, diaryContent);
+        User user = new User(userId, diaryDate, diaryContent);
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference();
-        myRef.child("users").child(userId).child("");
+
+        myRef.child("Users").child(userId).setValue(userId);
+        myRef.child("Users").child(userId).child("diaryContent").setValue(diaryContent);
+        myRef.child("Users").child(userId).child("diaryDate").setValue(diaryDate);
+
     }
 }

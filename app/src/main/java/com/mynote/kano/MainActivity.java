b8aabe11b0d9d2a43e5617d6f83444a539c30214
“jeongjiyoun <46271793+jeongjiyoun@users.noreply.github.com>
2019-06-20 23:45:53 +0900

    그런데 왜 데이터를 적용하지 못하지?
    어떻게 해결하지

--- app/src/main/java/com/mynote/kano/MainActivity.java
+++ app/src/main/java/com/mynote/kano/MainActivity.java
@@ -1,8 +1,11 @@
 package com.mynote.kano;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.apollographql.apollo.ApolloCall;
 import com.apollographql.apollo.ApolloClient;
@@ -15,6 +18,8 @@ import okhttp3.OkHttpClient;
 
 public class MainActivity extends AppCompatActivity {
 
+   private static Response<GetRepositoryQuery.Data> data;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,32 +29,36 @@ public class MainActivity extends AppCompatActivity {
         ApolloClient apolloClient = gitConnectApplication.getApolloClient();
         OkHttpClient okHttpClient = gitConnectApplication.getOkHttpClient();
 
-        if (okHttpClient != null) {
-            GetRepositoryQuery getRepositoryQuery
-                    = GetRepositoryQuery.builder()
-                    .loginId("MDQ6VXNlcjQ2MjcxNzkz").build();
+        GetRepositoryQuery getRepositoryQuery
+                = GetRepositoryQuery.builder()
+                .loginId("MDQ6VXNlcjQ2MjcxNzkz").build();
 
-            if (apolloClient != null) {
-                ApolloCall query = apolloClient
-                        .query(getRepositoryQuery);
+        ApolloCall query = apolloClient
+                .query(getRepositoryQuery);
 
-                if (query != null) {
-
-                    query.enqueue(new ApolloCall.Callback<GetRepositoryQuery.Data>() {
-
-                        @Override
-                        public void onResponse(@Nonnull Response<GetRepositoryQuery.Data> response) {
-                            String k = response.data().toString();
-                                Log.v("태그", k);
-                        }
-
-                        @Override
-                        public void onFailure(@Nonnull ApolloException e) {
-                            Log.e("1", e.getMessage(), e);
-                        }
-                    });
-                }
+        query.enqueue(new ApolloCall.Callback<GetRepositoryQuery.Data>() {
+            @Override
+            public void onResponse(@Nonnull Response<GetRepositoryQuery.Data> response) {
+                data = response;
+                String k = response.data().toString();
+                Log.v("태그", k);
             }
-        }
+
+            @Override
+            public void onFailure(@Nonnull ApolloException e) {
+                Log.e("1", e.getMessage(), e);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        String k = data.data().toString();
+        Log.v("태그", k);
+/*
+        TextView textView = findViewById(R.id.textView20);
+        textView.setText(data.data().toString());
+*/
     }
 }

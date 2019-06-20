31e3796917dd52bfd7bcf9918b67dbb9c317ab82
HJin90 <rooriim990@gmail.com>
2019-06-20 14:17:36 +0900

    11

---
+++ app/src/main/java/com/mynote/kano/RepositoryClicked.java
@@ -0,0 +1,13 @@
+package com.mynote.kano;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RepositoryClicked extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repository_clicked);
+    }
+}
